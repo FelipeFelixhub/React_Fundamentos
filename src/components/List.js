e787ed3028c7a6499c2e8371343cb37a2a2e82b7
Felipe Felix <felipe.dfelix@gmail.com>
2023-05-15 13:45:58 -0300

    Utilizando Props de uma forma avancada

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,15 @@
-import Item from "./Item"
+import Item from './Item'
 
 function List() {
     return (
         <>
           <h1>Minha Lista</h1>  
           <ul>
-            <Item marca="Ferrari"/>
-            <Item marca ="Porsche"/>
-            <Item marca ="VW"/>
+            <Item marca="Ferrari" ano_lancamento ={1985}/>
+            <Item marca ="Porsche" ano_lancamento ={2010}/>
+            <Item marca ="VW" ano_lancamento ={2023}/>
+            <Item marca="Chevrolet" ano_lancamento={1999}/>
+            <Item/>
           </ul>
         </>
     )
