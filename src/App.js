b473644dd08f8a49fa0771aed4a32d5f0a6dcfca
Felipe Felix <felipe.dfelix@gmail.com>
2023-05-15 11:06:18 -0300

    Trabalhando CSS e CSS Module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import SayMyName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
+import Frase from "./components/Frase";
 
 function App() {
   const nome = "Caroline";
 
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <Frase/>
       <SayMyName nome="Felipe" />
       <SayMyName nome="Henrique" />
       <SayMyName nome={nome} />
