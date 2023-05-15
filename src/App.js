a9b49936e158b2c6f6fccbfb49ad018a7c0ea20f
Felipe Felix <felipe.dfelix@gmail.com>
2023-05-15 09:51:59 -0300

    Criando componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
+import './App.css'
+import HelloWorld from './components/HelloWord'
+
 
 function App() {
+
+  const name = 'Henrique'
+
+  const newName = name.toUpperCase()
+
+  function sum(a,b) {
+    return a + b 
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro react</p>
+      <h2>Alterando o JSX</h2>
+      <p>Meu nome é {newName}</p>
+      <p>soma: {sum(1,2)}</p>
+      <img src={url} alt="Minha imagem" />
+      <HelloWorld/>
     </div>
-  );
-}
+  )
+  }
 
 export default App;
---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+    return (
+      <div>
+        <p>Este é um componenete com uma frase</p>
+      </div>  
+    )
+}
+
+export default Frase
