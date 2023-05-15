108d4c1465a2bf281f02cd1dbd4686a71885792c
Felipe Felix <felipe.dfelix@gmail.com>
2023-05-15 11:59:49 -0300

    Trabalhando fragments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import SayMyName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
 import Frase from "./components/Frase";
+import List from "./components/List";
 
 function App() {
   const nome = "Caroline";
@@ -20,6 +21,7 @@ function App() {
         profissao="Desenvolvedor Senior"
         foto="http://via.placeholder.com/150"
       />
+      <List/>
     </div>
   );
   }
